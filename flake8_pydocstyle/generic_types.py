ae95d5f5433efd81a226888925fa5a821b890aac
KRunchPL <krunchfrompoland@gmail.com>
2023-03-04 11:45:09 +0100

    Update dev dependencies and required Python version

--- flake8_pydocstyle/generic_types.py
+++ flake8_pydocstyle/generic_types.py
@@ -12,7 +12,7 @@ elif sys.version_info >= (3, 8):  # pragma: no cover (PY38+)
     from typing import Dict, Generator, List, Pattern, Tuple
 else:  # pragma: no cover (<PY38)
     from typing import Dict, Generator, List, Tuple
-    from typing.re import Pattern  # type: ignore
+    from typing.re import Pattern
 
 
 __all__ = ['Dict', 'Generator', 'List', 'Pattern', 'Tuple']
